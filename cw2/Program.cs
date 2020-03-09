b08630d19bf8eaffafdb47f5caf22a6b86075765
s18637 <s18637@LAB001-03>
2020-03-09 09:47:57 +0100

    dokonczyc

---
+++ cw2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace cw2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var path = @"C:\Users\s18637\Desktop\dane.csv";
+            var lines = File.ReadLines(path);
+            ICollection<Student> list = new List<Student>();
+            ICollection<string> log = new List<string>();
+            //dodać hash code i wlasny comparator
+            foreach (var line in lines)
+            {
+                var word = line.Split(",");
+                if (word.Length == 9)
+                {
+                    Student student = new Student(word[0], word[1], word[2], word[3], word[4], DateTime.Parse(word[5]), word[6], word[7], word[8]);
+                    if (!list.Contains(student))
+                    {
+                        list.Add(student);
+                    }
+                    else
+                    {
+                        log.Add(line);
+                    }
+                }
+                else
+                {
+                    log.Add(line);
+                }
+            }
+            FileStream writer = new FileStream(@"Z:\APBD\cw2\cw2\data\data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
+            serializer.Serialize(writer, list);
+            writer = new FileStream(@"Z:\APBD\cw2\cw2\data\log.txt", FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(writer);
+            string alllog="";
+            foreach (var line in log) {
+                alllog += line;
+            }
+            File.WriteAllText(@"Z:\APBD\cw2\cw2\data\log.txt", alllog);
+        }
+    }
+}
---
+++ cw2/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cw2
+{
+    public class Student
+    {
+        string fname { get; }
+        string lname { get; }
+        string kierunek { get; }
+        string type { get; }
+        string index { get; }
+        DateTime bdate { get; }
+        string mail { get; }
+        string mname { get; }
+        string dname { get; }
+        public Student(string fname, string lname, string kierunek, string type, string index, DateTime bdate, string mail, string mname, string dname)
+        {
+            this.fname=fname;
+            this.lname=lname;
+            this.kierunek=kierunek;
+            this.type=type;
+            this.index=index;
+            this.bdate=bdate;
+            this.mail=mail;
+            this.mname=mname;
+            this.dname=dname;
+        }
+    }
+}
