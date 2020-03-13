6b3cfcd5ebc1c9cf4414bd38afcb66a09bc1ae2d
s18637 <s18637@pjwstk.edu.pl>
2020-03-13 18:18:44 +0100

    done

--- cw2/Program.cs
+++ cw2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
@@ -9,9 +10,29 @@ namespace cw2
     {
         public static void Main(string[] args)
         {
+            //var sourcePath = "data.csv";
+            var sourcePath = @"C:\Users\Wojtek\Desktop\cw2\dane\dane.csv";
+            var desPath = "result.xml";
+            var type = "xml";
+            if (args.Length == 3)
+            {
+                sourcePath = args[0];
+                desPath = args[1];
+                type = args[2];
+            }
 
-            var path = @"C:\Users\s18637\Desktop\dane.csv";
-            var lines = File.ReadLines(path);
+            //var path = @"C:\Users\s18637\Desktop\dane.csv";
+            if(Uri.IsWellFormedUriString(sourcePath, UriKind.RelativeOrAbsolute) && Uri.IsWellFormedUriString(desPath, UriKind.RelativeOrAbsolute))
+            {
+                throw new ArgumentException("Podana sciezka jest nie prawidlowa");
+            }
+
+            if (File.Exists(sourcePath))
+            {
+                throw new FileNotFoundException("Podany plik nie istnieje");
+            }
+
+            var lines = File.ReadLines(sourcePath);
             ICollection<string> log = new List<string>();
             var hash = new HashSet<Student>(new OwnComaparer());
             foreach (var line in lines)
@@ -32,16 +53,25 @@ namespace cw2
                     log.Add(line);
                 }
             }
-            FileStream writer = new FileStream(@"Z:\APBD\cw2\cw2\data\data.xml", FileMode.Create);
-            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
-            serializer.Serialize(writer, hash);
-            writer = new FileStream(@"Z:\APBD\cw2\cw2\data\log.txt", FileMode.Create);
-            BinaryWriter bw = new BinaryWriter(writer);
-            string alllog="";
-            foreach (var line in log) {
-                alllog += line;
+            if (type.Equals("xml"))
+            {
+                FileStream writer = new FileStream(desPath, FileMode.Create);
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
+                serializer.Serialize(writer, hash);
+                
             }
-            File.WriteAllText(@"Z:\APBD\cw2\cw2\data\log.txt", alllog);
+            if (type.Equals("json"))
+            {
+                string jsonString = JsonConvert.SerializeObject(hash);
+                File.WriteAllText(desPath, jsonString);
+            }
+            string alllog = "";
+            foreach (var line in log)
+            {
+                    alllog += line;
+            }
+            Console.WriteLine(alllog);
+            File.WriteAllText(@"log.txt", alllog);
         }
     }
 }
